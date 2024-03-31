7ee96d01334f47227aa634537a7f60039ebf368d
OlesiaChirkova <chirkova.o@gmail.com>
2024-03-31 16:30:00 +0200

    toml updated

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,13 +1,15 @@
 import allure
 import pytest
-from config.base_test import BaseTest
+from cfg.base import BaseTest
 
 
 @allure.epic("Admin API")
 @allure.suite("Users API")
 class TestUsers(BaseTest):
 
+    @allure.title("Create, update, delete user")
     @pytest.mark.regression
+    @pytest.mark.api
     def test_create_update_delete_user(self):
         self.api_users.create_user()
         self.api_users.get_user_by_id(12)
